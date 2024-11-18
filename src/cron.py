31f7681790e5b1f72a62d19469d21a994c43ef0d
Strange <168190107+fxgurv@users.noreply.github.com>
2024-11-18 02:44:45 -0800

    Update cron.py

--- src/cron.py
+++ src/cron.py
@@ -1,65 +1,35 @@
-# RUN THIS N AMOUNT OF TIMES
 import os
 import sys
-
 from status import *
 from cache import get_accounts
 from config import get_verbose
-from classes.Twitter import Twitter
 from classes.YouTube import YouTube
 
 def main():
-    purpose = str(sys.argv[1])
-    account_id = str(sys.argv[2])
-
+    account_id = str(sys.argv[1])  # Now first argument is account_id directly
     verbose = get_verbose()
+    accounts = get_accounts("youtube")
 
-    if purpose == "twitter":
-        accounts = get_accounts("twitter")
-
-        if not account_id:
-            error("Account UUID cannot be empty.")
-
-        for acc in accounts:
-            if acc["id"] == account_id:
-                if verbose:
-                    info("Initializing Twitter...")
-                twitter = Twitter(
-                    acc["id"],
-                    acc["name"],
-                    acc["profile_path"],
-                    acc["topic"]
-                )
-                twitter.post()
-                if verbose:
-                    success("Done posting.")
-                break
-    elif purpose == "youtube":
-
-        accounts = get_accounts("youtube")
-
-        if not account_id:
-            error("Account UUID cannot be empty.")
-
-        for acc in accounts:
-            if acc["id"] == account_id:
-                if verbose:
-                    info("Initializing YouTube...")
-                youtube = YouTube(
-                    acc["id"],
-                    acc["name"],
-                    acc["profile_path"],
-                    acc["niche"],
-                    acc["language"]
-                )
-                youtube.generate_video()
-                youtube.upload_video()
-                if verbose:
-                    success("Uploaded Short.")
-                break
-    else:
-        error("Invalid Purpose, exiting...")
+    if not account_id:
+        error("Account UUID cannot be empty.")
         sys.exit(1)
+
+    for acc in accounts:
+        if acc["id"] == account_id:
+            if verbose:
+                info("Initializing YouTube...")
+            youtube = YouTube(
+                acc["id"],
+                acc["name"],
+                acc["profile_path"],
+                acc["niche"],
+                acc["language"]
+            )
+            youtube.generate_video()
+            youtube.upload_video()
+            if verbose:
+                success("Uploaded Short.")
+            break
 
 if __name__ == "__main__":
     main()
